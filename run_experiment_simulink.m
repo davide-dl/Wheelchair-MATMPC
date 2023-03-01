0192a218e42037099ba05a8232e58dca003e5e7e
davidedl <davidedl@davidedl-OptiPlex-7020>
2023-03-01 17:31:57 +0100

    Added and tested scripts to run experiments with gazebo.

--- run_experiment_simulink.m
+++ run_experiment_simulink.m
@@ -1,27 +1,30 @@
 % Reset matlab
-% Initialization_Simulink
+rosshutdown
+Initialization_Simulink
 rosinit
-node_matlab_sig = ros.Node('/matlab_sig1');
+node_matlab_sig = ros.Node('/matlab_simulink_sig');
 
 % Send signal to record
 signalstart = ros.Publisher(node_matlab_sig, "/start_record_wheelchair", ...
     "std_msgs/Float32", "IsLatching", true);
 
 msg_record_start = rosmessage(signalstart);
-msg_record_start.Data = single(100.1);
+t = rostime('now','DataFormat','struct');
+msg_record_start.Data = single(t.Sec + t.Nsec*10^-9);
 send(signalstart, msg_record_start);
 
-zref = 2;
+zref = 4;
 yref = 1;
 
 % Start simulation
-% sim('WMR_ROS_MATMPC_SIMULINK', 20)
+sim('WMR_ROS_MATMPC_SIMULINK', 20)
 
 % Send signal to stop record
 signalstop = ros.Publisher(node_matlab_sig, "/stop_record_wheelchair", ...
     "std_msgs/Float32", "IsLatching", true);
 msg_record_stop = rosmessage(signalstop);
-msg_record_stop.Data = single(200.2);
+t = rostime('now','DataFormat','struct');
+msg_record_stop.Data = single(t.Sec + t.Nsec*10^-9);
 send(signalstop, msg_record_stop);
 
 rosshutdown
